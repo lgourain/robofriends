4ec61427ba618fcacb86184bfda1a399414f8bf1
Louis Gourain <lgourain@wanadev.fr>
2020-02-28 11:39:28 +0100

    Updates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("/robofriends/precache-manifest.a96ad22b8bf3411d493416d983b77247.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/robofriends/precache-manifest.8593962d7f46ac9af00b574f0b5eaf04.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 import {BackgroundSyncPlugin, Queue} from 'workbox-background-sync';
 import {registerRoute} from 'workbox-routing';
