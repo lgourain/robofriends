8de4262f0f776def2908cf1bef353aeefdc57e94
Louis Gourain <lgourain@wanadev.fr>
2020-02-28 11:21:19 +0100

    Updates

--- precache-manifest.2b2260ba4b014ff5120ca0b4d9ac03e9.js
+++ precache-manifest.2b2260ba4b014ff5120ca0b4d9ac03e9.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9145f6928f07ece79d6d832cc611c886",
+    "revision": "be780940266e8ecb59b3423636100458",
     "url": "/robofriends/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
   },
   {
-    "revision": "e70a7e9c1d3e1b6b82ed",
+    "revision": "d6773935201906bcea66",
     "url": "/robofriends/static/css/main.13a72664.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/robofriends/static/js/2.b97c5154.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e70a7e9c1d3e1b6b82ed",
-    "url": "/robofriends/static/js/main.96004ae0.chunk.js"
+    "revision": "d6773935201906bcea66",
+    "url": "/robofriends/static/js/main.07f02ae6.chunk.js"
   },
   {
     "revision": "d6ceab57e5523008bfa1",
